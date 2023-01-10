6ec6f06c7c764ffd3a9bc6bfd6d492deac474648
horatiu84 <horatiul@yahoo.com>
2023-01-10 16:29:01 +0200

    adding photos in the main App component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "Jackets",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Sneackers",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "Women",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Men",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map((category) => (
-        <div className="category-container">
-          <div className="backgound-image"></div>
+      {categories.map(({ id, title, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
-            <h2>{category.title}</h2>
+            <h2>{title}</h2>
             <p>Shop now!</p>
           </div>
         </div>
