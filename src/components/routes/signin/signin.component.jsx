3f0e4d1ea22013872c03fb565a111598fe6ec255
horatiu84 <horatiul@yahoo.com>
2023-01-16 15:36:53 +0200

    adding firebase doc configuration

--- src/components/routes/signin/signin.component.jsx
+++ src/components/routes/signin/signin.component.jsx
@@ -1,9 +1,10 @@
 import { signInWithGooglePopup } from "../../../utils/firebase/firebase";
+import { createUserDocumentFromAuth } from "../../../utils/firebase/firebase";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
-    console.log(response);
+    createUserDocumentFromAuth(response.user);
   };
 
   return (
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -6,6 +6,8 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBFUhTxWftts2mZfKz719il03U_1KVyoq4",
@@ -26,5 +28,14 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+};
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
